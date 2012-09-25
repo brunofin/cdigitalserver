482703776b47bc76f0dc459c74dafcc4fd0fc56c
Bruno Finger <brunofin@users.noreply.github.com>
2012-09-25 04:45:09 +0000

    Modificações no modo que a conexão funciona (orientada a dispositivo). Adicionada a classe de identificação do dispositivo Android (server.Device).

--- src/server/conexao/Connection.java
+++ src/server/conexao/Connection.java
@@ -5,9 +5,21 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+import server.Device;
+
+import bean.*;
+
+/**
+ * Representa uma conexão entre o Servidor e o Cliente
+ * @author bruno
+ *
+ */
 public class Connection implements Runnable {
 	private Socket cliente;
 	private ConnectionManager servidor;
+	private Device device;
+	private ObjectInputStream in;
+	private ObjectOutputStream out;
 	
 	public Connection(Socket cliente, ConnectionManager servidor) {
 		this.cliente = cliente;
@@ -19,20 +31,38 @@ public class Connection implements Runnable {
 		try {
 			iniciar();
 		} catch(Exception e) {
-			
+			System.out.println("[server]<Connection>: Exceção iniciando! " + e.getMessage() );
 		}
 		
 	}
 	
 	private void iniciar() throws IOException, ClassNotFoundException {
-		ObjectInputStream in = new ObjectInputStream(cliente.getInputStream());
-		ObjectOutputStream out = new ObjectOutputStream(cliente.getOutputStream());
-		System.out.println("Conexão: Conexão iniciada!");
+		in = new ObjectInputStream(cliente.getInputStream());
+		out = new ObjectOutputStream(cliente.getOutputStream());
+		
+		
+		device = (Device) in.readObject();
+		
+		System.out.println("[server]<Connection>: Conexão iniciada: " + device.getAndroid_id() + " : " + device.getMesa());
 		
 		while(true) {
-			String s = (String) in.readObject();
-			System.out.println("Mensagem recebida: " + s);
+			Object comando = in.readObject();
+			
+			if(comando instanceof String) {
+				String s = (String) comando;
+				if(s.startsWith("shutdown")) {
+					break;
+				} else {
+					System.out.println("[server]<Connection>: Mensagem recebida: " + s);
+				}
+			} else if(comando instanceof Pedido) {
+				// mais alguma coisa
+			} else {
+				// continuar a lista de possiveis ações
+			}
 		}
+		System.out.println("[server]<Connection>: Finalizando servidor...");
+		servidor.finalizarServidor();
 	}
 
 	public Socket getCliente() {
@@ -49,6 +79,17 @@ public class Connection implements Runnable {
 
 	public void setServidor(ConnectionManager servidor) {
 		this.servidor = servidor;
+	}
+	
+	public void fecharConexao() {
+		try {
+			in.close();
+			out.close();
+			cliente.close();
+		} catch(IOException e) {
+			System.out.println("[servidor]<Connection> Exceção finalizando! " + e.getMessage());
+		}
+		
 	}
 
 }
--- src/server/conexao/ConnectionManager.java
+++ src/server/conexao/ConnectionManager.java
@@ -6,43 +6,66 @@ import java.net.Socket;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Abre as portas para criar conexões e então insere uma representação da conexão em uma
+ * lista. É basicamente o servidor
+ * @author bruno
+ * @see Connection
+ */
 public class ConnectionManager implements Runnable {
 	private List<Connection> conexoes;
+	private boolean serverAlive;
 	
 	public ConnectionManager() {
 		conexoes = new LinkedList<Connection>();
+		serverAlive = true;
 	}
 	@Override
 	public void run() {
 		try {
 			iniciar();
 		} catch(IOException e) {
-			
+			System.out.println("[server]<ConnectionManager>: Exceção! " + e.getMessage());
 		}
 		
 	}
 	
 	private void iniciar() throws IOException {
-		@SuppressWarnings("resource") //ver o próximo TODO e depois deletar esta linha.
 		ServerSocket server = new ServerSocket(4445);
 		
-		System.out.println("Esperando clientes...");
-		
-		while(true) { //TODO: depois mudar o true pra uma opção na GUI que possa parar o servidor. 
+		while(isServerAlive()) {
+			System.out.println("[server]<ConnectionManager> Esperando novos clientes...");
 			Socket cliente = server.accept();
-			System.out.println("Servidor: Nova conexão recebida!");
 			
+			System.out.println("[server]<ConnectionManager> Nova conexão recebida!");
 			Connection c = new Connection(cliente, this);
 			
 			new Thread(c).start();
 			getConexoes().add(c);
 		}
-		//server.close();
+		System.out.println("[server]<ConnectionManager>: Não aceitando novos clientes.");
+		server.close();
 	}
+	
+	public void finalizarServidor() {
+		System.out.println("[server]<ConnectionManager>: Finalizando servidor...");
+		serverAlive = false;
+		for(Connection c : conexoes) {
+			c.fecharConexao();
+		}
+		conexoes.clear();
+		System.out.println("[server]<ConnectionManager>: Servidor finalizado com sucesso!");
+	}
+	
 	public List<Connection> getConexoes() {
 		return conexoes;
 	}
+	
 	public void setConexoes(List<Connection> conexoes) {
 		this.conexoes = conexoes;
+	}
+	
+	public boolean isServerAlive() {
+		return serverAlive;
 	}
 }
