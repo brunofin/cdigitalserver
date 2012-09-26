e56fa9c7c9ce623ea491eb3c9ac2f8bc07044b4b
gpasar <gpasar@1dbf2d15-ac5a-4bcd-ad51-ef16b93a548b>
2012-09-26 02:44:16 +0000

    criado o método preparaTabelas

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,22 +1,78 @@
 package main;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import bean.Tipo;
+
 import server.conexao.ConnectionManager;
+
 import controle.CtrJanelaInicio;
+import dao.factory.DAOFactory;
+import dao.factory.Database;
+import dao.tipo.TipoDAO;
 
 /* Lista geral de o que falta fazer
  * TODO: fazer algo com as Exception quando usa try/catch (invés de usar um bloco vazio).
  */
 
 public class Main {
-
+	static DAOFactory factory;
+	static TipoDAO tipoDAO;
+	
 	public static void main(String[] args) {
 		/* CtrJanelaInicio janelaInicio = new CtrJanelaInicio();
-		 * janelaInicio.executar();
-		 */
-		
-		Thread servidor = new Thread(new ConnectionManager());
-		
-		servidor.start();
-		
+		 *janelaInicio.executar();
+		 
+		 
+		 Thread servidor = new Thread(new ConnectionManager());
+		 
+		 servidor.start();
+ 		 */
+
+		prepararTabelas();
+	}
+	
+	/**
+	 * Método chamado ao iniciar aplicação
+	 * para criar tabelas e fazer os inserts
+	 * necessários.
+	 */
+	private static void prepararTabelas() {
+		factory = DAOFactory.getDaoFactory(Database.MYSQL);
+		tipoDAO = factory.getTipoDAO();
+		try {
+			tipoDAO.criarTabela();
+		} catch (SQLException e) {
+			System.out.println("erro ao criar tabela tipo "+e);
+		}
+		consultarEInserirPratoBebida(1, "Bebida");
+		consultarEInserirPratoBebida(2, "Prato");
+	}
+	
+	/**
+	 * Consulta se existe os tipos prato e bebida na tabela tipo,
+	 * caso não existam insere.
+	 * @param tipoId, id do tipo
+	 * @param nome, nome do tipo
+	 */
+	private static void consultarEInserirPratoBebida(int tipoId, String nome) {
+		Tipo t = new Tipo();
+		t.setTipoId(tipoId);
+		t.setNome(nome);
+		List <Tipo> tipos= null;
+		try {
+			tipos=tipoDAO.consultarTitulo(t);
+		} catch (SQLException e) {
+			System.out.println("Erro ao consultar tipos por título: "+e);
+		}
+		//caso nao tenha tipo com o nome pesquisado (Prato, Bebida) insere
+		if(tipos.isEmpty()){
+			try {
+				System.out.println(tipoDAO.incluir(t)==1?"inseriu novo tipo":"não inseriu novo tipo");
+			} catch (SQLException e) {
+				System.out.println("erro ao inserir prato. "+e);
+			}
+		}
 	}
 }
