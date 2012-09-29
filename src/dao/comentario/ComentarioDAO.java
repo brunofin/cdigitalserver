f91cca5be011a932eaa1bd83f961f86038d48ee9
gpasar <gpasar@1dbf2d15-ac5a-4bcd-ad51-ef16b93a548b>
2012-09-29 05:34:42 +0000

    Criação de novos métodos no ComentarioDAO, falta testar tudo.

--- src/dao/comentario/ComentarioDAO.java
+++ src/dao/comentario/ComentarioDAO.java
@@ -8,9 +8,72 @@ import bean.Comentario;
 public interface ComentarioDAO {
 	public int incluir(Comentario c) throws SQLException;
     public boolean exluir(Comentario c) throws SQLException;
+    /**
+     * Altera somente o atributo comentário.
+     * 
+     * @param c, comentario a ser alterado
+     * @return
+     * @throws SQLException
+     */
     public boolean alterar(Comentario c) throws SQLException;
-    public Comentario consultarId(Comentario c) throws SQLException;
-    public List<Comentario> consultarTitulo(Comentario c) throws SQLException;
+    /**
+     * Consulta comentarios de um Item,
+     * para apresenta-los no cardápio
+     * os comentários da lista só contem:
+     * id do comentário, timeInMillis,
+     * o texto do comentario e o nome do
+     * Cliente que fez o comentário.
+     * 
+     * @param c, comentário apenas com
+     * id do Item preenchido.
+     * @return lista de comentários de um
+     * Item
+     * @throws SQLException
+     */
+    public List<Comentario> consultarComentariosItem(Comentario c) throws SQLException;
+    /**
+     * Consulta todos os comentários de um 
+     * cliente, os comentários vem com data,
+     * o texto do comentário, e o nome do item 
+     * que foi comentado
+     * 
+     * @param c, comentário com apenas o 
+     * id do cliente preenchido
+     * @return lista de comentários de um cliente
+     * @throws SQLException
+     */
+    public List<Comentario> consultarComentariosCliente(Comentario c) throws SQLException;
+    /**
+     * Exclui todos os comentário de um cliente
+     * que será excluído
+     * 
+     * @param c, comentario com apenas o
+     * id do cliente que será excluido
+     * @return true se excluiu os comentários
+     * @throws SQLException
+     */
+    public boolean excluirComentariosClienteExcluido(Comentario c)throws SQLException;
+    /**
+     * Exclui todos os comentarios de um 
+     * item.
+     * Deve ser chamado ao excluir o item,
+     * para que todos os seus comentários sejam 
+     * excluídos
+     * 
+     * @param c, comentario com somente 
+     * o id do item que será excluído.
+     * @return
+     * @throws SQLException
+     */
+    public boolean excluirTodosComentariosDeItemExcluido(Comentario c)throws SQLException;
+    /**
+     * Retorna uma lista com todos os comentários
+     * do sistema, vem preenchidos apenas o id do,
+     * comentário, a data e o texto do comentário.
+     *  
+     * @return lista de todos os comentários.
+     * @throws SQLException
+     */
     public List<Comentario> listar() throws SQLException;
     public void criarTabela() throws SQLException;
 }
--- src/dao/comentario/MySqlComentarioDAO.java
+++ src/dao/comentario/MySqlComentarioDAO.java
@@ -1,8 +1,15 @@
 package dao.comentario;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
+import bean.Cliente;
 import bean.Comentario;
 import dao.factory.MySqlDAOFactory;
 
@@ -10,45 +17,178 @@ public class MySqlComentarioDAO extends MySqlDAOFactory implements
 		ComentarioDAO {
 
 	@Override
-	public int incluir(Comentario c) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int incluir(Comentario c) throws SQLException {//TODO testar
+		Connection con = getConnection();
+        Statement stmt =  con.createStatement();
+        int resultado = stmt.executeUpdate("INSERT INTO comentario " +
+        				"(id_comentario,data,comentario,id_item,id_cliente) " +
+        				"VALUES ("+c.getComentarioId()+","
+        				+c.getData().getTimeInMillis()+
+        				",'"+c.getComentario()+
+        				"',"+c.getItem().getItemId()+
+        				","+c.getCliente().getId()+")");
+        stmt.close();
+        con.close();
+        return resultado;
 	}
 
 	@Override
-	public boolean exluir(Comentario c) throws SQLException {
-		// TODO Auto-generated method stub
+	public boolean exluir(Comentario c) throws SQLException {//TODO testar
+		Connection con = getConnection();
+		Statement stmt =  con.createStatement();
+		int resultado = stmt.executeUpdate("DELETE FROM comentario WHERE id_comentario=" 
+							+c.getComentarioId());
+		stmt.close();
+		con.close();
+		if(resultado==1){
+			return true;
+		}
 		return false;
 	}
 
 	@Override
-	public boolean alterar(Comentario c) throws SQLException {
-		// TODO Auto-generated method stub
+	public boolean alterar(Comentario c) throws SQLException {//TODO testar
+		Connection con = getConnection();
+        PreparedStatement stmt = con.prepareStatement
+        		("UPDATE comentario SET comentario = ?" +
+                " WHERE id_comentario = ?");
+        stmt.setString(1, c.getComentario());
+        stmt.setInt(2, c.getComentarioId()); 
+        
+        int modificou=stmt.executeUpdate();
+        stmt.close();
+        con.close();
+        if(modificou==1){
+            return true;
+        }
+        return false;
+	}
+
+	@Override
+	public List<Comentario> listar() throws SQLException {//TODO testar
+		Comentario resultado = null;
+		List <Comentario> comentarios = new ArrayList<Comentario>();
+		Connection con = getConnection();
+        ResultSet rs = null;
+        Statement stmt =  con.createStatement();
+        rs = stmt.executeQuery("SELECT id_comentario, data, comentario FROM comentario;");
+        while (rs.next()){
+        	resultado = new Comentario();
+        	resultado.setComentarioId(rs.getInt("id_comentario"));
+        	Calendar dataComentario = Calendar.getInstance();
+        	dataComentario.setTimeInMillis(rs.getLong("data"));
+        	resultado.setData(dataComentario);
+        	resultado.setComentario(rs.getString("comentario"));
+        	
+            comentarios.add(resultado);
+        }
+        stmt.close();
+        con.close();
+        return comentarios;
+	}
+
+	@Override
+	public void criarTabela() throws SQLException {//TODO testar
+		Connection con = getConnection();
+		Statement stmt =  con.createStatement();
+		stmt.execute("CREATE TABLE IF NOT EXISTS comentario (" +
+                " id_comentario INTEGER (7) AUTO_INCREMENT NOT NULL," +
+                " data BIGINT (13) NOT NULL," +
+                " comentario VARCHAR (160) NOT NULL," +
+                " id_item INTEGER (7) NOT NULL," +
+                " id_cliente INTEGER (7) NOT NULL," +
+                " PRIMARY KEY (id_comentario)," +
+                " FOREIGN KEY (id_item) REFERENCES item (id_item)," +
+                " FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente))");
+		stmt.close();
+		con.close();
+	}
+
+	@Override
+	public List<Comentario> consultarComentariosItem(Comentario c) throws SQLException {//TODO testar
+		Comentario resultado = null;
+		List <Comentario> comentarios = new ArrayList<Comentario>();
+		Connection con = getConnection();
+        ResultSet rs = null;
+        Statement stmt =  con.createStatement();
+        rs = stmt.executeQuery("SELECT co.id_comentario, " +
+        		"co.data, co.comentario, c.nome" +
+        		" FROM comentario AS co INNER JOIN cliente AS c WHERE " +
+        		"co.id_cliente=c.id_cliente AND" +
+        		" co.id_item = "+c.getItem().getItemId());
+        while (rs.next()){
+        	resultado = new Comentario();
+        	resultado.setComentarioId(rs.getInt("id_comentario"));
+        	Calendar dataComentario = Calendar.getInstance();
+        	dataComentario.setTimeInMillis(rs.getLong("data"));
+        	resultado.setData(dataComentario);
+        	resultado.setComentario(rs.getString("comentario"));
+        	resultado.setCliente(new Cliente());
+        	resultado.getCliente().setNome(rs.getString("nome"));
+        	
+            comentarios.add(resultado);
+        }
+        stmt.close();
+        con.close();
+        return comentarios;
+	}
+
+	@Override
+	public List<Comentario> consultarComentariosCliente(Comentario c)
+			throws SQLException {//TODO testar
+		Comentario resultado = null;
+		List <Comentario> comentarios = new ArrayList<Comentario>();
+		Connection con = getConnection();
+        ResultSet rs = null;
+        Statement stmt =  con.createStatement();
+        //rs = stmt.executeQuery("SELECT * FROM comentario WHERE id_cliente="+c.getCliente().getId());
+        rs = stmt.executeQuery("SELECT co.id_comentario, co.data, co.comentario, i.nome AS nome_do_item FROM" +
+        		" comentario AS co INNER JOIN item AS i ON co.id_item=i.id_item WHERE co.id_item = "+c.getCliente().getId());
+        		
+        while (rs.next()){
+        	resultado = new Comentario();
+        	resultado.setComentarioId(rs.getInt("id_comentario"));
+        	Calendar dataComentario = Calendar.getInstance();
+        	dataComentario.setTimeInMillis(rs.getLong("data"));
+        	resultado.setData(dataComentario);
+        	resultado.setComentario(rs.getString("comentario"));
+        	resultado.setCliente(new Cliente());
+        	resultado.getItem().setNome(rs.getString("nome_do_item"));
+        	
+            comentarios.add(resultado);
+        }
+        
+        return comentarios;
+	}
+
+	@Override
+	public boolean excluirComentariosClienteExcluido(Comentario c)
+			throws SQLException {//TODO testar
+		Connection con = getConnection();
+		Statement stmt =  con.createStatement();
+		int resultado = stmt.executeUpdate("DELETE FROM comentario WHERE id_cliente=" 
+							+c.getCliente().getId());
+		stmt.close();
+		con.close();
+		if(resultado==1){
+			return true;
+		}
 		return false;
 	}
 
 	@Override
-	public Comentario consultarId(Comentario c) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Comentario> consultarTitulo(Comentario c) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Comentario> listar() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void criarTabela() throws SQLException {
-		// TODO Auto-generated method stub
-
+	public boolean excluirTodosComentariosDeItemExcluido(Comentario c)
+			throws SQLException {//TODO testar
+		Connection con = getConnection();
+		Statement stmt =  con.createStatement();
+		int resultado = stmt.executeUpdate("DELETE FROM comentario WHERE id_item=" 
+							+c.getItem().getItemId());
+		stmt.close();
+		con.close();
+		if(resultado==1){
+			return true;
+		}
+		return false;
 	}
 
 }
