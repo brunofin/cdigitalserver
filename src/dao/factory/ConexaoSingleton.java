7daa27c7e919c8c4f1ae0e646db15c23595557d3
Bruno Finger <brunofin@users.noreply.github.com>
2012-10-09 16:24:09 +0000

    dao.factory.ConexaoSingleton agora lê configurações do servidor pelo arquivo servidor.cfg

--- src/dao/factory/ConexaoSingleton.java
+++ src/dao/factory/ConexaoSingleton.java
@@ -9,38 +9,40 @@ import servidor.Configuracao;
 
 public class ConexaoSingleton {
 	private static Connection con = null;
-	private static final String jdbcURL;	// localhost:3306
-    private static final String usuario;	// root
-    private static final String senha;	// root
-    
-    static {
-    	try{
-            Class.forName("com.mysql.jdbc.Driver");
-        }catch(ClassNotFoundException e){
-        	System.out.println("driver não encontrado "+e);
-        }
-    	
-    	Configuracao cfg = new Configuracao();
-    	
-    	try {
-    		cfg.ler();
-    	} catch(IOException | ClassNotFoundException e) {
-    		System.out.println("<ConexaoSingleton> Erro ao ler configurações: " + e.getMessage());
-    	}
-    	
-    	usuario = cfg.getDbUsuario();
-    	senha = cfg.getDbSenha();
-    	jdbcURL = "jdbc:mysql://" + cfg.getDbIp() + ":" + cfg.getDbPorta() + "/tcc";
-    }
-    
+	private static final String jdbcURL; // localhost:3306
+	private static final String usuario; // root
+	private static final String senha; // root
+
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("driver não encontrado " + e);
+		}
+
+		Configuracao cfg = new Configuracao();
+
+		try {
+			cfg.ler();
+		} catch (IOException | ClassNotFoundException e) {
+			System.out.println("<ConexaoSingleton> Erro ao ler configurações: "
+					+ e.getMessage());
+		}
+
+		usuario = cfg.getDbUsuario();
+		senha = cfg.getDbSenha();
+		jdbcURL = "jdbc:mysql://" + cfg.getDbIp() + ":" + cfg.getDbPorta()
+				+ "/tcc";
+	}
+
 	public static final Connection getConexao() {
-		if(con == null){
+		if (con == null) {
 			try {
-	        	con = DriverManager.getConnection(jdbcURL,usuario,senha);
-	            System.out.println("conectado pelo ConexaoSingleton");
-	        }catch( SQLException e ){ 
-	        	System.out.println("falha no driver "+e); 
-	        }
+				con = DriverManager.getConnection(jdbcURL, usuario, senha);
+				System.out.println("conectado pelo ConexaoSingleton");
+			} catch (SQLException e) {
+				System.out.println("<ConexaoSingleton> Falha no driver: " + e.getMessage());
+			}
 		}
 		return con;
 	}
